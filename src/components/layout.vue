67fe816b4d45b0125c6cdda9167fd1cd465aa684
Abincat <sss831020@gmail.com>
2023-11-11 10:58:24 +0800

    add

---
+++ src/components/layout.vue
@@ -0,0 +1,41 @@
+<template>
+    <main class="main-block">
+        <div class="main">
+            <section class="section section-left">
+                <navbar :show="navBarSow"></navbar>
+                <div class="left-expand" ref="leftExpand">
+                    <p class="statement">
+                        ©本網站為緯育TibaMe第87期前端工程師專業技術養成班學員作品,僅供學習、展示之用途。
+                    </p>
+                    <button class="arrowBtn" @click="show">
+                        <img src="../assets/images/icon/right-arrow.svg" alt="">
+                    </button>
+                </div>
+            </section>
+            <section class="section-right">
+                <slot name="section-right-content"></slot>
+            </section>
+        </div>
+    </main>
+</template>
+<script>
+//import 這頁需要的元件
+import navbar from '@/components/navBar.vue'
+
+export default {
+    components: {
+        navbar
+    },
+    data() {
+        return {
+            navBarSow: true,
+        };
+    },
+    methods: {
+        show() {
+            this.$refs.leftExpand.classList.toggle("left-expand-off")
+            this.navBarSow = !this.navBarSow;
+        }
+    }
+}
+</script>
---
+++ src/components/navBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav :class="['nav', { 'nav-off': !show }]" ref="navBar">
+        <div>
+            <router-link :to="{ name: 'home' }" class="nav-brandlogo">
+                <img src="../assets/images/logo/logo_desk.svg" alt="logo" />
+            </router-link>
+        </div>
+        <div class="nav-items">
+            <router-link :to="{ name: 'chatting_room' }">
+                <img src="../assets/images/icon/chat.svg" alt="chat icon" />
+                <h3>聊天大廳</h3>
+            </router-link>
+            <router-link :to="{ name: 'playground' }">
+                <img src="../assets/images/icon/game.svg" alt="game icon" />
+                <h3>遊樂場</h3>
+            </router-link>
+            <router-link :to="{ name: 'activity' }">
+                <img src="../assets/images/icon/activity.svg" alt="activity icon" />
+                <h3>主題活動</h3>
+            </router-link>
+            <router-link :to="{ name: 'about-us' }">
+                <img src="../assets/images/icon/about-us.svg" alt="about-us icon" />
+                <h3>關於叩叩</h3>
+            </router-link>
+            <router-link :to="{ name: 'service' }">
+                <img src="../assets/images/icon/qa.svg" alt="service icon" />
+                <h3>我有問題</h3>
+            </router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: ['show'],
+}
+</script>
